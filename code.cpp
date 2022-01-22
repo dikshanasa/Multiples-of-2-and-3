9602a8af028a32bc7fe61218cbc3c48fead43c0b
Diksha Nasa <64835443+dikshanasa@users.noreply.github.com>
2022-01-22 10:56:40 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,11 @@
+int FindSum(int *arr,int l)
+{
+    int sum=0;
+    for(int i=0;i<l;i++){
+        if(arr[i]%2==0 || arr[i]%3==0){
+            sum=sum+arr[i];
+        }
+    }
+    return sum;
+//Write code here 
+}
